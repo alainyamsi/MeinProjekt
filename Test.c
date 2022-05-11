8b3aac93caf02afe99c0e3c1acc72532c4db98b6
Herman <xtb-mtd-u1@fh-h.de>
2022-05-11 11:16:05 +0200

    commit 1

--- Test.c
+++ Test.c
@@ -4,8 +4,8 @@ int main ()
 {
 	prinf("Haloo World");
 	printf("Ich ahbe was eingefügt");
-		printf("LoL ich habe keine Ahnung!");
-	printf("doch ich habe  Ahnung!");
+	printf("LoL ich habe keine Ahnung!");
+	printf("doch wir habe  Ahnung!");
 
 	
 }
