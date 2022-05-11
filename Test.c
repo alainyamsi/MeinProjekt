d3f7947d4728e9ab8f400ff37ebef8650542cbcc
alainyamsi <105341230+alainyamsi@users.noreply.github.com>
2022-05-11 11:11:34 +0200

    Update Test.c
    du

--- Test.c
+++ Test.c
@@ -5,7 +5,7 @@ int main ()
 	prinf("Haloo World");
 	printf("Ich ahbe was eingefügt");
 		printf("LoL ich habe keine Ahnung!");
-	printf("doch ich habe  Ahnung!");
+	printf("doch du habe  Ahnung!");
 
 	
 }
